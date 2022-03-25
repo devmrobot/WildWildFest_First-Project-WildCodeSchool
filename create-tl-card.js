bb559faa05cd77ad16b6365ba08478bd4425e5b3
Thomas K <thmskffr@gmail.com>
2022-03-25 10:05:52 +0100

    trying to build a flex display of artits

--- create-tl-card.js
+++ create-tl-card.js
@@ -1,26 +1,96 @@
+const artists = [
+    {
+        name: "Elvis Presley",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "/assets/images/artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    },
+    {
+        name: "Chuck Berry",
+        time: "Mer. 22 Octobre 2022 - 16h30",
+        image: "./assets/images/Artists/elvis.jpeg",
+        description: "dfklzejflkezjfdklezjflkezjdfkezjfkzejfkljezklfjzelkfjelkfjlkezfjzekljfeklzjflkezfjklezjflkzejflkezjflkjzelfjezklfjkljezflkjzelfjzelkjflkezjflkezjfljezfklezjflkezjflezjfelzjfklezjf",
+    }
+]
 function createCard(name, time, image, description) {
-    const cardList = document.querySelector(".timeline ul");
+
+    const cardList = document.querySelector("ul.list-container");
     const newCard = document.createElement("li");
-    cardList.appendChild(newCard);
+    
 
     const cardBody = document.createElement("div");
+    cardBody.classList.add("card");
     newCard.appendChild(cardBody);
 
     const cardImage = document.createElement("img");
     cardImage.classList.add("artist-img");
     cardBody.appendChild(cardImage);
-    cardImage.src = url(image);
+    cardImage.src = `${image}`
 
     const cardTitle = document.createElement("h2");
-    cardTime.classList.add("artist-name");
+    cardTitle.classList.add("artist-name");
     cardBody.appendChild(cardTitle);
     cardTitle.innerHTML = name;
 
     const cardTime = document.createElement("h3");
     cardTime.classList.add("artist-time");
+    cardTime.innerHTML = time;
     cardBody.appendChild(cardTime);
 
     const cardDescription = document.createElement("p");
     cardDescription.classList.add("artist-description");
+    cardDescription.innerHTML = description;
     cardBody.appendChild(cardDescription);
+
+    cardList.appendChild(newCard);
 }
+for (let i = 0; i < artists.length; i++) {
+    createCard(
+      artists[i].name,
+      artists[i].time,
+      artists[i].image,
+      artists[i].description
+    );
+  }
